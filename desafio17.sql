9242479091880d1684b3967f0ee2ad502745f8f5
ericvini <ericcobain3@gmail.com>
2021-03-21 12:48:55 -0300

    subindo projeto para o GitHub

---
+++ desafio17.sql
@@ -0,0 +1,2 @@
+SELECT supplier_id, id FROM northwind.purchase_orders
+WHERE supplier_id = 1 OR supplier_id = 3 OR supplier_id = 5 OR supplier_id = 7;
---
+++ desafio7.sql
@@ -0,0 +1,1 @@
+select id from northwind.products order by id desc limit 5;
